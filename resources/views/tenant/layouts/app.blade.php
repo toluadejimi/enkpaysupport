18d2d6102e0827c83d679d0bfafd9cc09a09b0d3
Jimmy <toluadejimi@gmail.com>
2024-06-23 02:04:38 +0100

    nnjjkkkkkkk

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -61,5 +61,20 @@ All Jquery Script link
 @include('tenant.layouts.script')
 @stack('script')
 
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6346d5b854f06e12d899cdc8/1gf6b5nf1';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 </html>
