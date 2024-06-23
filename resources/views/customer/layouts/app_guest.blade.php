1f05b5777937d3e42c28d1ac35c18307eeaf8f77
Jimmy <toluadejimi@gmail.com>
2024-06-23 02:16:20 +0100

    nnjjkkkkkkk

--- resources/views/customer/layouts/app_guest.blade.php
+++ resources/views/customer/layouts/app_guest.blade.php
@@ -90,6 +90,19 @@
 @stack('script')
 <script src="{{ asset('frontend/assets/js/custom.js') }}"></script>
 
+
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6346d5b854f06e12d899cdc8/1gf6b5nf1';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+
 </body>
 
 </html>
